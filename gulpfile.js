ce7850ddcbdf2332bba5b98fd851496cd5bdc857
Epick362 <flp.hajek@gmail.com>
2016-04-22 00:02:49 +0200

    Gulp Dist

--- gulpfile.js
+++ gulpfile.js
@@ -81,3 +81,4 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['appJS', 'appViews', 'vendorCSS', 'vendorJS', 'fonts', 'sass', 'watch']);
+gulp.task('dist', ['appJS', 'appViews', 'vendorCSS', 'vendorJS', 'fonts', 'sass']);
